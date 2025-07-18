85776fee07bb5117d391f6930c08c5db3be8a84d
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2025-07-18 13:52:41 -0700

    Refactor(tabby): use single delegated click handler on tab container

--- src/main.js
+++ src/main.js
@@ -1,24 +1,26 @@
-// select all tab buttons and covert to an array
-const tabs = Array.from(document.querySelectorAll(".tab"))
-// select the tabby
 const tabby = document.querySelector('.tabby')
-//select the tab contents
-const tabContents = Array.from(tabby.querySelectorAll('.tab-content'))
+const tabsList = tabby.querySelector('.tabs')
+const tabs = Array.from(tabby.querySelectorAll('.tab')) //creates an array of tabs
+const tabContents = Array.from(tabby.querySelectorAll('.tab-content')) //creates an arrat if tab contents
 
 
-// add a click event listener to each tabl button
+tabsList.addEventListener('click', e => {
+        //use event target to find the clicked tab
+        const tab = e.target
+        console.log(tab)
+        //Get the corresponding tab-content through data-target attribute (data-theme)
+        const target = tab.dataset.theme
+        const tabContent= tabby.querySelector('#' + target)
+      
+        //remove is-selected from all the tabs
+        //must be an array to loop using forEach
+        tabs.forEach(t => t.classList.remove('is-selected'))
+        //add is-selected to the clicked tab
+        tab.classList.add('is-selected')
 
-tabs.forEach(tab => {
-   tab.addEventListener('click', e => {
-    const theme = tab.dataset.theme
-    const tabContent = tabby.querySelector('#' + theme)
-
-    tabs.forEach(t => t.classList.remove('is-selected'))
-    tab.classList.add('is-selected')
-
-    tabContents.forEach(c => c.classList.remove('is-selected'))
-    tabContent.classList.add('is-selected')
-   })
+        //remove is-selected from all target tabContent
+        //must be an array to loop using forEach
+        tabContents.forEach(c => c.classList.remove('is-selected'))
+        //add is-selected to the target tabContent
+        tabContent.classList.add('is-selected')
 })
-
-
